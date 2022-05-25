bd8977b6063c760b8efd386167e6d0f315a2a25d
Jinmo Gu <phdnc99@gmail.com>
2022-05-25 14:57:48 -0400

    final version

--- Coursera AssignW3.R
+++ Coursera AssignW3.R
@@ -1,13 +1,25 @@
 makeVector<-function(x=numeric()){
-    m<-NULL
-    set<-function(y){
-      x<<-y
-      m<<-NULL
-    }
-    get<-function() x
-    setmean<-function(mean) m<<-mean()
-    getmean<-function() m
-    list(set=set, get=get,
-         setmean=setmean,
-         getmean=getmean)
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setmean<-function(mean) m<<-mean()
+  getmean<-function() m
+  list(set=set, get=get,
+       setmean=setmean,
+       getmean=getmean)
+}
+
+cacheinverse<-function(x,...) {
+  inv<-x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  matrix_to_invert<-x$get()
+    inv<-solve(matrix_to_invert, ...)
+    x$setinverse(inv)
+    inv
 }
